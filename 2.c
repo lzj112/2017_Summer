ada9f2f3621979575df54716fed147f5a77714e6
lzj112 <872575628@qq.com>
2017-07-18 14:07:33 +0800

    07.18

--- 2.c
+++ 2.c
@@ -2,19 +2,38 @@
 	> File Name: 2.c
 	> Author: 
 	> Mail: 
-	> Created Time: 2017年07月17日 星期一 15时35分25秒
+	> Created Time: 2017年07月18日 星期二 10时14分59秒
  ************************************************************************/
 
 #include<stdio.h>
 int main()
 {
-    int n,a,b;
-    scanf("%d",&n);
-    while(n)
+    int n,i,j,t,sum;
+    float avg,b,c;
+    int a[100];
+    while(scanf("%d",&n)!=EOF)
     {
-        n--;
-        scanf("%d %d",&a,&b);
-        printf("%d\n",a+b);
+        sum=0;
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++)
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+        for(i=1;i<n-1;i++)
+        sum+=a[i];
+        n=n-2;
+        b=sum*1.0,c=n*1.0;
+        avg=b/c;
+        printf("%.2f\n",avg);
     }
     return 0;
 }
--- 3.c
+++ 3.c
@@ -2,18 +2,45 @@
 	> File Name: 3.c
 	> Author: 
 	> Mail: 
-	> Created Time: 2017年07月17日 星期一 15时39分27秒
+	> Created Time: 2017年07月18日 星期二 10时46分20秒
  ************************************************************************/
 
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    while(scanf("%d %d",&a,&b)!=EOF)
-    {
-        if(a=0&&b=0)
-        break;
-        printf("%d\n",a+b);
+    int n,m,sum,a[100],avg,flag;
+    while(scanf("%d %d",&n,&m)!=EOF)
+    { 
+        int i,j;
+        for(i=0,j=1;i<n;i++,j++)
+        {
+            a[i]=2*j;
+        }
+        int t=n%m;
+        for(i=0;i<n-t;i=i+m)
+        {
+            sum = 0;
+            flag = 0;
+            for(j=i;flag<m;flag++,j++)
+            {
+                sum+=a[j];
+            }
+            avg=sum/m;
+            if(i==0)
+            printf("%d",avg);
+            else
+            printf(" %d",avg);
+        }
+        sum=0;
+        if(t!=0)
+        {
+            int tmp=n-i;
+        for(;i<n;i++)
+        sum+=a[i];
+        avg=sum/tmp;
+        printf(" %d",avg);
+        }
+        printf("\n");
     }
-    return 0;
+        return 0;
 }
